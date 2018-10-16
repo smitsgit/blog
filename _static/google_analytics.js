9ad764eab98d5442f509dd6988ad9871015da8d3
Smital Desai <smital.desai@go-jek.com>
2018-10-16 08:29:01 +0530

    test analytics yet again

--- _static/google_analytics.js
+++ _static/google_analytics.js
@@ -1,2 +1,7 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
-gtag('config', 'UA-120801664-1');
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-120801664-1"></script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-120801664-1');
